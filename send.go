df45654f09ed60b60e42d82d893f38eaec2cfb0d
Niko Köser <koeserniko@gmail.com>
2023-07-27 18:34:15 +0200

    chore(notify): extract send config related code to send_config.go

--- send.go
+++ send.go
@@ -2,7 +2,6 @@ package notify
 
 import (
 	"context"
-	"io"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -30,37 +29,4 @@ func (n *Notify) Send(ctx context.Context, subject, message string, opts ...Send
 // It performs these operations concurrently and returns the first encountered error, if any.
 func Send(ctx context.Context, subject, message string, opts ...SendOption) error {
 	return defaultNotify.Send(ctx, subject, message, opts...)
-}
-
-// SendConfig is used to configure the Send call.
-type SendConfig interface {
-	// SetAttachments sets attachments that can be sent alongside the message.
-	SetAttachments(attachments ...Attachment)
-	// SetMetadata sets additional metadata that can be sent with the message.
-	SetMetadata(metadata map[string]any)
-}
-
-// SendOption is a function that modifies the configuration of a Send call.
-type SendOption = func(SendConfig)
-
-// Attachment represents a file that can be attached to a notification message.
-type Attachment interface {
-	// Reader is used to read the contents of the attachment.
-	io.Reader
-	// Name is used as the filename when sending the attachment.
-	Name() string
-}
-
-// SendWithAttachments attaches the provided files to the message being sent.
-func SendWithAttachments(attachments ...Attachment) SendOption {
-	return func(c SendConfig) {
-		c.SetAttachments(attachments...)
-	}
-}
-
-// SendWithMetadata attaches the provided metadata to the message being sent.
-func SendWithMetadata(metadata map[string]any) SendOption {
-	return func(c SendConfig) {
-		c.SetMetadata(metadata)
-	}
 }
---
+++ send_config.go
@@ -0,0 +1,38 @@
+package notify
+
+import "io"
+
+// SendConfig is used to configure the Send call.
+type SendConfig interface {
+	// SetAttachments sets attachments that can be sent alongside the message.
+	SetAttachments(attachments ...Attachment)
+
+	// SetMetadata sets additional metadata that can be sent with the message.
+	SetMetadata(metadata map[string]any)
+}
+
+// SendOption is a function that modifies the configuration of a Send call.
+type SendOption = func(SendConfig)
+
+// Attachment represents a file that can be attached to a notification message.
+type Attachment interface {
+	// Reader is used to read the contents of the attachment.
+	io.Reader
+
+	// Name is used as the filename when sending the attachment.
+	Name() string
+}
+
+// SendWithAttachments attaches the provided files to the message being sent.
+func SendWithAttachments(attachments ...Attachment) SendOption {
+	return func(c SendConfig) {
+		c.SetAttachments(attachments...)
+	}
+}
+
+// SendWithMetadata attaches the provided metadata to the message being sent.
+func SendWithMetadata(metadata map[string]any) SendOption {
+	return func(c SendConfig) {
+		c.SetMetadata(metadata)
+	}
+}
